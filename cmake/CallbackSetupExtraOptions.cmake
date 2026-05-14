40be83d687aef84145e312f530e4bf2cb23439b7
zps <zps@ornl.gov>
2026-05-13 21:21:40 -0400

    CallbackSetupExtraOptions.cmake — added IF(EXISTS ...) guard,

--- cmake/CallbackSetupExtraOptions.cmake
+++ cmake/CallbackSetupExtraOptions.cmake
@@ -2,7 +2,9 @@
 ## testframework/cmake/CallbackSetupExtraOptions.cmake
 ##---------------------------------------------------------------------------##
 MACRO(TRIBITS_REPOSITORY_SETUP_EXTRA_OPTIONS)
-  ADD_SUBDIRECTORY(${testframework_SOURCE_DIR}/setup)
+  IF(EXISTS "${testframework_SOURCE_DIR}/setup")
+    ADD_SUBDIRECTORY(${testframework_SOURCE_DIR}/setup)
+  ENDIF()
 ENDMACRO()
 ##---------------------------------------------------------------------------##
 ##       end of testframework/cmake/CallbackSetupExtraOptions.cmake
