ded296f990204e88fd3cf9aa5aab6c9c3a038f91
Jordan P. Lefebvre <jlefebvre@aerisllc.com>
2026-06-05 20:36:57 +0000

    Cleanup Qt dependency

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -1,13 +1,13 @@
-IF(USE_QT6)
-  SET(QT_DEP Qt6Widgets Qt6Core)
-ELSEIF(USE_QT5)
-  SET(QT_DEP Qt5Widgets Qt5Core)
-ENDIF()
+IF(TPL_ENABLE_Qt6Core OR TPL_ENABLE_Qt5Core)
+  IF(USE_QT6)
+    SET(QT_DEP Qt6Widgets Qt6Core)
+  ELSEIF(USE_QT5)
+    SET(QT_DEP Qt5Widgets Qt5Core)
+  ENDIF()
 
-# allow for tests to be disabled and no testing harness to be compiled.
-IF(${PROJECT_NAME}_ENABLE_TESTS)
+  # allow for tests to be disabled and no testing harness to be compiled.
   TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
-    LIB_REQUIRED_TPLS_OPTIONAL ${QT_DEP}
+    LIB_REQUIRED_TPLS ${QT_DEP}
   )
 ELSE()
   TRIBITS_PACKAGE_DEFINE_DEPENDENCIES()
